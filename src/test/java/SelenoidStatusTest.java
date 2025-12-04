02437b30c5a67a04ada78aa14f5c80878d08ada5
artl <artemlarichev.v@gmail.com>
2025-12-04 11:36:39 +0100

    add SelenoidStatusTest (Rest Assured demo)

---
+++ src/test/java/SelenoidStatusTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class SelenoidStatusTest {
+
+    @Test
+    void checkTotalIs5() {
+        given()
+                .log().all() // логируем запрос
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().body()      // логируем ответ
+                .statusCode(200)   // проверяем статус
+                .body("total", equalTo(5)); // проверяем поле total
+    }
+    @Test
+    void checkTotalWithoutLog() {
+        given()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .body("total", is(5));
+    }
+
+}
